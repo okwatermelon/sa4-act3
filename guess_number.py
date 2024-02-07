e7bba3884b700be77b7d9a5af39bf8b194f909c2
Anthony Ybarra <ovfan@outlook.com>
2024-02-06 20:37:50 -0600

    Made a limited number of guesses that the user can make, and implemented guess counter

--- guess_number.py
+++ guess_number.py
@@ -1,11 +1,12 @@
 number = 10
-
+count = 5
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
-while guess != 'q':
+guess = int(input(f"What number am I thinking of? You get {count} guesses! "))
+while guess != 'q' and count != 1:
     if guess == number:
         print("Congratulations! You guessed the right number.")
         guess = 'q'
     else:
-        guess = int(input("Try again! (or q to quit) "))
+        count -= 1
+        guess = int(input(f"{count} guess(es) left, Try again! (or q to quit) "))
         
